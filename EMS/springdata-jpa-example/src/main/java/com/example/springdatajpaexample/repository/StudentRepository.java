7ae14447c8dd1be48a8ceefdba820ebdfd99aea1
User <impubudu@gmail.com>
2019-10-07 18:53:37 +0530

    10/7

--- EMS/springdata-jpa-example/src/main/java/com/example/springdatajpaexample/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springdatajpaexample.repository;
-
-import com.example.springdatajpaexample.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-}
